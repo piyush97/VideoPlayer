09e273d7ab32efa4d4973e97434f1c22a3b3ca06
Piyush Mehta <me@piyushmehta.com>
2018-04-06 12:08:53 +0530

    gave access to express

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const express = require('express');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+const api = require('./server/routes/api');
+const port = 3000;
+const app = express();
+app.use(express.static(path.join(_dirname, 'dist')));
