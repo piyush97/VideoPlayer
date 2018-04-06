86f0c6eca217362956346db80f54943d5655a831
Piyush Mehta <me@piyushmehta.com>
2018-04-06 21:48:50 +0530

    Error throwing func added

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,13 +4,21 @@ const mongoose = require('mongoose');
 const Video = require('../models/video');
 const db = "mongodb://piyush:1234@ds237409.mlab.com:37409/videoplayer";
 mongoose.Promise = global.Promise;
-mongoose.connect(db,function(err){
-  if(err){
-    console.error("Error!"+err);
+mongoose.connect(db, function(err) {
+  if (err) {
+    console.error("Error!" + err);
   }
 });
 
-router.get('/', function(req, req) {
-  res.send('API WORKS!!!');
+router.get('/videos', function(req, req) {
+  console.log("Get request for all videos");
+  Video.find({})
+    .exec(function(err, videos) {
+      if (err) {
+        console.log("Error retrieving videos")
+      } else {
+        res.json(videos);
+      }
+    });
 });
 module.exports = router;
