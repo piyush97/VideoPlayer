f4810988cb434c289ab25ed93ae37a27c431260c
Piyush Mehta <me@piyushmehta.com>
2018-04-06 21:33:06 +0530

    Mongoose added

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+
 router.get('/', function(req, req) {
   res.send('API WORKS!!!');
 })
